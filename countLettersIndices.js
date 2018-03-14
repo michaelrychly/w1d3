3bc5689ce2c03f4e97d5f359c84c5431088c94f9
michaelrychly <michaelrychly@gmail.com>
2018-03-14 17:42:57 +0000

    added solutiong countLettersIndices.js

--- countLettersIndices.js
+++ countLettersIndices.js
@@ -1,22 +1,40 @@
 function countLetters(sentence){
+  var letterObject = {};
+  var test = [1, 2];
 
-  var withoutSpaces = sentence.split(" ").join("").toLowerCase();;
-  var letterObject = { };
+  for (var i = 0; i < sentence.length; i++) {
+    var letter = sentence[i];
+    var indices = new Array;
 
-  for (var i = 0; i < withoutSpaces.length; i++) {
-    var letter = withoutSpaces[i];
+    for (var j = 0; j < sentence.length; j++) {
 
-    if(letterObject[letter] === undefined){
-      letterObject[letter] = 1;
+      if (sentence[j] === letter && sentence[j] !== " "){
+        indices += (indices.length === 0 ? "" : ", ") + j;
+      }
     }
-    else{
-      letterObject[letter] += 1;
+
+    if(letterObject[letter] === undefined && sentence[i] !== " "){
+      letterObject[letter] = indices;
     }
   }
 
   for (var letter in letterObject){
-    console.log(letter + " " + letterObject[letter]);
+      console.log(letter + " " + letterObject[letter]);
   }
 }
 
 console.log(countLetters("lighthouse in the house"));
+
+//lighthouse in the house
+//01234567890123456789012
+//letter indices
+//l 0
+//i 1, 11
+//g 2
+//h 3, 5, 15, 18
+//t 4, 14
+//o 6, 19
+//u 7, 20
+//s 8, 21
+//e 9, 16, 22
+//n 12
