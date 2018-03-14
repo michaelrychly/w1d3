6fd8f94b3fd9232ab94cf40b2323b99fc7a300a4
michaelrychly <michaelrychly@gmail.com>
2018-03-14 16:07:01 +0000

    add solution countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,1 +1,22 @@
-hello
+function countLetters(sentence){
+
+  var withoutSpaces = sentence.split(" ").join("").toLowerCase();;
+  var letterObject = { };
+
+  for (var i = 0; i < withoutSpaces.length; i++) {
+    var letter = withoutSpaces[i];
+
+    if(letterObject[letter] === undefined){
+      letterObject[letter] = 1;
+    }
+    else{
+      letterObject[letter] += 1;
+    }
+  }
+
+  for (var letter in letterObject){
+    console.log(letter + " " + letterObject[letter]);
+  }
+}
+
+console.log(countLetters("lighthouse in the house"));
